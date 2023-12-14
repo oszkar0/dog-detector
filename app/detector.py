7043d289ce202d815b788fdc5919bc9c1838369f
oszkar0 <oskszysiak@gmail.com>
2023-12-14 01:05:09 +0100

    inital commit for detector app

---
+++ app/detector.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, GlobalMaxPooling2D
+
+
